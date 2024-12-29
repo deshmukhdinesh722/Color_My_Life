d1c122e84bfa7c023f7364ef7f3f4c079351f523
Dinesh Deshmukh <131383340+deshmukhdinesh722@users.noreply.github.com>
2024-12-29 12:42:16 +0530

    Update checkAuth.jsx

--- frontend/src/components/common/checkAuth.jsx
+++ frontend/src/components/common/checkAuth.jsx
@@ -4,6 +4,18 @@ import { Navigate, useLocation } from 'react-router-dom'
 function CheckAuth({isAuthenticated, user, children}) {
     
     const location =useLocation() // specifies the currentr path
+        if (location.pathname === "/") {
+        if (!isAuthenticated) {
+          return <Navigate to="/auth/login" />;
+        } else {
+          if (user?.role === "admin") {
+            return <Navigate to="/admin/dashboard" />;
+          } else {
+            return <Navigate to="/user-page/home" />;
+          }
+        }
+      }
+
     if(!isAuthenticated && !(location.pathname.includes('/login') || location.pathname.includes('/register'))){ 
 
         // yamule jr authenticted nasel tr login page vr redirect honar
